717de8596a5654f2ab449b4fe3f59e4fafea0ba6
Trevor Jameson <tjameson15@gmail.com>
2018-06-12 21:14:35 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,4 +3,5 @@
 
 def display_board(moves)
   board = " #{moves[0]} | #{moves[1]} | #{moves[2]} \n-----------\n #{moves[3]} | #{moves[4]} | #{moves[5]} \n-----------\n #{moves[6]} | #{moves[7]} | #{moves[8]} "
+  puts board
 end
